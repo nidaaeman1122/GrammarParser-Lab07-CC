9696219e54a8919f695bc70d6f3772d1ddb81906
nidaeman1122 <nidaeman0987@gmail.com>
2024-11-06 05:10:05 -0800

    Add project files.

---
+++ GrammarParser/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GrammarParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.parseButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(292, 169);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // parseButton
+            // 
+            this.parseButton.Location = new System.Drawing.Point(423, 169);
+            this.parseButton.Name = "parseButton";
+            this.parseButton.Size = new System.Drawing.Size(75, 23);
+            this.parseButton.TabIndex = 1;
+            this.parseButton.Text = "Parse";
+            this.parseButton.UseVisualStyleBackColor = true;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(381, 228);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(134, 13);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "result is correct or incorrect";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.parseButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button parseButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ GrammarParser/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GrammarParser
+{
+    public partial class Form1 : Form
+    {
+        private int index = 0; // Tracks the position of the input string
+        private string input = ""; // Input string to be parsed
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Add the Form1_Load event handler
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // You can initialize form components here if needed.
+        }
+
+        // Grammar S → E
+        private bool S()
+        {
+            return E();
+        }
+
+        // Grammar E → E + T | T
+        private bool E()
+        {
+            int startPos = index;
+
+            if (T())
+            {
+                if (index < input.Length && input[index] == '+')
+                {
+                    index++;
+                    if (E()) return true;
+                }
+                else
+                {
+                    return true; // Return true as T is valid
+                }
+            }
+
+            index = startPos; // Backtrack
+            return false;
+        }
+
+        // Grammar T → T * F | F
+        private bool T()
+        {
+            int startPos = index;
+
+            if (F())
+            {
+                if (index < input.Length && input[index] == '*')
+                {
+                    index++;
+                    if (T()) return true;
+                }
+                else
+                {
+                    return true; // Return true as F is valid
+                }
+            }
+
+            index = startPos; // Backtrack
+            return false;
+        }
+
+        // Grammar F → ( E ) | id
+        private bool F()
+        {
+            int startPos = index;
+
+            if (index < input.Length && input[index] == '(')
+            {
+                index++;
+                if (E())
+                {
+                    if (index < input.Length && input[index] == ')')
+                    {
+                        index++;
+                        return true;
+                    }
+                }
+            }
+            else if (index < input.Length && char.IsLetter(input[index])) // id is represented by a letter
+            {
+                index++;
+                return true;
+            }
+
+            index = startPos; // Backtrack
+            return false;
+        }
+
+        // This method is triggered when the "Parse" button is clicked
+        private void parseButton_Click(object sender, EventArgs e)
+        {
+            input = inputTextBox.Text.Replace(" ", ""); // Remove spaces for easier parsing
+            index = 0; // Reset index
+
+            if (S() && index == input.Length) // Check if the entire input matches
+            {
+                resultLabel.Text = "Valid input.";
+                resultLabel.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                resultLabel.Text = "Invalid input.";
+                resultLabel.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
